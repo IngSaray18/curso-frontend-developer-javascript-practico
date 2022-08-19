8c0d4694c415a67c78071b65b7a7e9a00cb1f6ed
IngSaray18 <pedrosaray18@hotmail.com>
2022-08-18 19:35:47 -0500

    agregro el mobile menu

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-const menuEmail  = document.querySelector(".navbar-email");
-const  desktoMenu = document.querySelector(".desktop-menu");
-menuEmail.addEventListener("click", toggleDesktopMenu);
+const menuEmail = document.querySelector(".navbar-email");
+const desktoMenu = document.querySelector(".desktop-menu");
+const menuBurguerIcon = document.querySelector(".menu");
+const menuBurguer = document.querySelector(".mobile-menu");
 
-function toggleDesktopMenu(){
-desktoMenu.classList.toggle("inactive")
-};
+
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuBurguerIcon.addEventListener("click", toggleBurguer);
+
+
+function toggleDesktopMenu() {
+  desktoMenu.classList.toggle("inactive");
+}
+function toggleBurguer() {
+  menuBurguer.classList.toggle("inactive");
+}
